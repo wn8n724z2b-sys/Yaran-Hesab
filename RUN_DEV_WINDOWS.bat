84cf5be6839960d75ac853c0dbd80b5e02efdb9f
wn8n724z2b-sys <wn8n724z2b@privaterelay.appleid.com>
2026-09-19 20:32:06 +0430

    Delete RUN_DEV_WINDOWS.bat

--- RUN_DEV_WINDOWS.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d %~dp0
-where npm >nul 2>nul || (echo Node.js/npm is not installed. & pause & exit /b 1)
-where rustc >nul 2>nul || (echo Rust is not installed. Run scripts\SETUP_WINDOWS.ps1 first. & pause & exit /b 1)
-if not exist node_modules call npm install
-call npm run dev
